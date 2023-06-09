9d9f4bb6f126043611b772dbc9d0b64e95ef3277
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-10 00:13:06 +0530

    mongo-add

---
+++ main.js
@@ -0,0 +1,18 @@
+
+import { MongoClient } from "mongodb";
+
+async function main() {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  // Executing the Query
+  const db = client.db("mydbs");
+  const messageColl = db.collection("message");
+  await messageColl.insertOne({ message: "HI I am Node!" });
+
+  // closing conn.
+  await client.close();
+  console.log("Record Added!");
+}
+
+main();
